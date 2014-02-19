699032817a3b5de51b267ec0f75f20045176726c
Gordon Lesti <info@gordonlesti.com>
2014-02-19 15:43:06 +0100

    add fpc to request_processors

---
+++ app/code/community/Lesti/Fpc2/Model/Fpc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gordon
+ * Date: 2/19/14
+ * Time: 3:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Lesti_Fpc2_Model_Fpc
+{
+    public function extractContent($content = false)
+    {
+        // TODO return cached page
+    }
+}
