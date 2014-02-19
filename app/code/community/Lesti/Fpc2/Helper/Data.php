df0c4d99b54abb78a64a3bad011a034ea73380d0
Gordon Lesti <info@gordonlesti.com>
2014-02-19 15:56:26 +0100

    add Helper

---
+++ app/code/community/Lesti/Fpc2/Helper/Data.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: gordon
+ * Date: 2/19/14
+ * Time: 3:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class Lesti_Fpc2_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    
+}
